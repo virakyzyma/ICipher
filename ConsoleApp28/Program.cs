a1ac8a3f73ccf41e4c5e5938b789d824cc379fa8
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-03-21 14:04:27 +0200

    Add project files.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string text = "Hello World!";
+            Cipher cipher = new Cipher();
+
+            string encryptedStr = cipher.Encode(text, 1);
+            string decodeStr = cipher.Decode(encryptedStr, 1);
+
+            Console.WriteLine($"Original string: {text}");
+            Console.WriteLine($"Encrypted string: {encryptedStr}");
+            Console.WriteLine($"Original string: {decodeStr}");
+        }
+        public interface ICipher
+        {
+            string Encode(string str, int key);
+            string Decode(string str, int key);
+        }
+        public class Cipher : ICipher
+        {
+            public string Encode(string str, int key)
+            {
+                string encryptedString = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    encryptedString += (char)(str[i] + key);
+                }
+                return encryptedString;
+            }
+            public string Decode(string str, int key)
+            {
+                string decryptedString = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    decryptedString += (char)(str[i] - key);
+                }
+                return decryptedString;
+            }
+        }
+    }
+}
